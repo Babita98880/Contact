aca197dc4333c5af1ad5557d9bdc4bf61d1726f6
Babita <a00280963@mycambrian.ca>
2023-08-12 20:24:48 -0400

    add userDefault storage

--- Contact/EntryViewController.swift
+++ Contact/EntryViewController.swift
@@ -11,8 +11,10 @@ class EntryViewController: UIViewController {
     @IBOutlet var nameField: UITextField!
     @IBOutlet var emailField: UITextField!
     @IBOutlet var phoneField: UITextField!
+    var contacts: [Contact] = []
     public var completion :((String, String, String)-> Void)?
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         nameField.becomeFirstResponder()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
@@ -21,11 +23,14 @@ class EntryViewController: UIViewController {
                emailField.placeholder = "Email (Optional)"
                phoneField.placeholder = "Phone"
     }
-    @objc func didTapSave(){
-        if let name = nameField.text, !name.isEmpty,
-           let phone = phoneField.text, !phone.isEmpty {
-            completion?(name, emailField.text ?? "", phone)
-        }
-    }
+
+    @objc func didTapSave() {
+         if let name = nameField.text, !name.isEmpty,
+            let phone = phoneField.text, !phone.isEmpty {
+             let newContact = Contact(name: name, email: emailField.text ?? "", mobileNumber: phone)
+             contacts.append(newContact) // Append the new contact to the items array
+             completion?(name, emailField.text ?? "", phone)
+         }
+     }
    
 }
--- Contact/ViewController.swift
+++ Contact/ViewController.swift
@@ -5,33 +5,49 @@
 //  Created by Babita Rawat on 2023-08-12.
 //
 import UIKit
-
+//Declaration
+struct Contact: Codable {
+    var name: String
+    var email: String
+    var mobileNumber:String
+}
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var table: UITableView!
     @IBOutlet var label: UILabel!
-    var models: [(name: String, email: String, mobileNumber: String)] = []
-    
+    var contacts: [Contact] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Contacts"
         table.delegate = self
         table.dataSource = self
-        loadInitialContacts()
+        loadStoredContacts()
     }
     
     // Load initial contacts
-    func loadInitialContacts() {
-//        models = [
-//            ("John Doe", "john@example.com", "123-456-7890"),
-//            ("Jane Smith", "jane@example.com", "987-654-3210")
-//        ]
-//        table.reloadData()
-        updateEmptyState()
+    func loadStoredContacts() {
+        contacts.removeAll()
+        if let storedData = UserDefaults.standard.data(forKey: "items") {
+                   do {
+                       contacts = try JSONDecoder().decode([Contact].self, from: storedData)
+//                                   for x in 0..<contacts.count {
+//                                               let newContact = contacts[x]
+//                                       self.contacts.append(Contact(name: newContact.name, email: newContact.email, mobileNumber: newContact.mobileNumber))
+//                                               label.isHidden = true
+//                                               table.isHidden = false
+//
+//                                 }
+                       updateEmptyState()
+                       table.reloadData()
+                   } catch let err {
+                       print("Error decoding stored items: \(err)")
+                   }
+               }
+           
     }
     
     // Update empty state message
     func updateEmptyState() {
-        if models.isEmpty {
+        if contacts.isEmpty {
             label.text = "No contacts yet"
             label.isHidden = false
             table.isHidden = true
@@ -40,37 +56,48 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             table.isHidden = false
         }
     }
-
     @IBAction func didTapNewContact() {
         if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "new") as? EntryViewController {
             vc.title = "New Contact"
             vc.navigationItem.largeTitleDisplayMode = .never
-            
+            vc.contacts = contacts // Pass the items array to EntryViewController
             vc.completion = { [weak self] contactName, contactEmail, contactPhone in
                 guard let self = self else { return }
                 vc.navigationController?.popToRootViewController(animated: true)
-                self.models.append((contactName, contactEmail, contactPhone))
+                self.contacts.append(Contact(name: contactName, email: contactEmail, mobileNumber: contactPhone))
+//                self.contacts.append((contactName, contactEmail, contactPhone))
                 self.updateEmptyState()
                 self.table.reloadData()
+                self.saveItemsToUserDefaults() // Save the updated items array
             }
             navigationController?.pushViewController(vc, animated: true)
         }
     }
 
+
+
+    // Save items to UserDefaults
+        func saveItemsToUserDefaults() {
+            if let encoded = try? JSONEncoder().encode(contacts) {
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
+        }
+
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return models.count
+        return contacts.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = models[indexPath.row].name
-        cell.detailTextLabel?.text = models[indexPath.row].mobileNumber
+        cell.textLabel?.text = contacts[indexPath.row].name
+        cell.detailTextLabel?.text = contacts[indexPath.row].mobileNumber
         return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let model = models[indexPath.row]
+        let model = contacts[indexPath.row]
         if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "contact") as? ContactViewController {
             vc.title = "Contact"
             vc.nameTitle = model.name
@@ -80,4 +107,61 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             navigationController?.pushViewController(vc, animated: true)
         }
     }
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { [weak self] (_, _, completionHandler) in
+            self?.editContact(at: indexPath)
+            completionHandler(true)
+        }
+        editAction.backgroundColor = .blue
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, completionHandler) in
+            self?.deleteContact(at: indexPath)
+            completionHandler(true)
+        }
+        
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction, editAction])
+        return configuration
+    }
+
+    func editContact(at indexPath: IndexPath) {
+        let contact = contacts[indexPath.row]
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "new") as? EntryViewController {
+            vc.title = "Edit Contact"
+            vc.navigationItem.largeTitleDisplayMode = .never
+            vc.nameField.text = contact.name
+            vc.emailField.text = contact.email
+            vc.phoneField.text = contact.mobileNumber
+            vc.completion = { [weak self] contactName, contactEmail, contactPhone in
+                guard let self = self else { return }
+                vc.navigationController?.popToRootViewController(animated: true)
+                // Create a new Contact instance with the updated data
+                let updatedContact = Contact(name: contactName, email: contactEmail, mobileNumber: contactPhone)
+                self.contacts[indexPath.row] = updatedContact // Update the array
+                self.table.reloadData()
+                self.updateEmptyState()
+            }
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+    func deleteContact(at indexPath: IndexPath) {
+        contacts.remove(at: indexPath.row)
+        table.deleteRows(at: [indexPath], with: .fade)
+        updateEmptyState()
+        saveContactsToUserDefaults()
+    }
+    func saveContactsToUserDefaults() {
+        var contactsDict: [String: [String: Any]] = [:]
+        for (index, contact) in contacts.enumerated() {
+            let contactDict: [String: Any] = [
+                "name": contact.name,
+                "email": contact.email,
+                "phone": contact.mobileNumber
+            ]
+            contactsDict["contact_\(index)"] = contactDict
+            print(contactsDict)
+        }
+        UserDefaults.standard.set(contactsDict, forKey: "contacts")
+    }
+
 }
